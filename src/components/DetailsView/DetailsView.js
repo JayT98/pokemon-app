a6efd90b401ac2c6b335957a58af7ab7f60148e1
JayT98 <kientrung2509@gmail.com>
2023-05-06 05:18:18 +1000

    create overlay and add into Details view

--- src/components/DetailsView/DetailsView.js
+++ src/components/DetailsView/DetailsView.js
@@ -1,19 +1,14 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import { PokemonCard } from "../PokemonCard";
 import { Details } from "../Details";
 import { BackButton } from "../BackButton";
+import { Overlay } from "../Overlay";
 import "./DetailsView.css";
 
-function DetailsView({ pokemon, setSelectedPokemon}) {
+function DetailsView({ pokemon, setSelectedPokemon }) {
     const detailsViewRef = useRef(null);
-
-    // useEffect(() => {
-    //     // Disable scrolling on mount
-    //     document.body.style.overflow = "hidden";
-    //     // Enable scrolling on unmount
-    //     return () => (document.body.style.overflow = "auto");
-    // }, []);
+    const [isHideOverlay, setIsHideOverlay] = useState(false);
 
     useEffect(() => {
         document.body.classList.add("no-scroll");
@@ -23,62 +18,26 @@ function DetailsView({ pokemon, setSelectedPokemon}) {
     }, []);
 
     const handleBackClick = () => {
+        setIsHideOverlay(true);
+
         detailsViewRef.current.classList.add("hidden");
+
         setTimeout(() => {
             setSelectedPokemon(-1);
         }, 500);
     };
 
     return ReactDOM.createPortal(
-        <div className="details-view-container shown" ref={detailsViewRef}>
-            <BackButton onClick={handleBackClick} />
-            <PokemonCard pokemon={pokemon} />
-            <Details pokemon={pokemon} />
+        <div>
+            <Overlay hidden={isHideOverlay} onClick={handleBackClick} />,
+            <div className="details-view-container shown" ref={detailsViewRef}>
+                <BackButton onClick={handleBackClick} />
+                <PokemonCard pokemon={pokemon} />
+                <Details pokemon={pokemon} />
+            </div>
         </div>,
         document.body
     );
 }
 
 export default DetailsView;
-// import React, { Component, createRef } from 'react';
-// import ReactDOM from 'react-dom';
-// import BackButton from '../BackButton/BackButton';
-// import { PokemonCard } from '../PokemonCard';
-// import { Details } from '../Details';
-// import './DetailsView.css';
-
-// class DetailsView extends Component
-// {
-//     constructor( props )
-//     {
-//         super( props );
-//         this.detailsViewRef = createRef( null );
-
-//         this.handleBackClick = this.handleBackClick.bind( this );
-//     }
-
-//     // handle back button clicking
-//     handleBackClick()
-//     {
-//         this.detailsViewRef.current.classList.add( 'hidden' );
-
-//         setTimeout( () => {
-//             this.props.setSelectedPokemon( -1 );
-//         }, 500 );
-//     }
-
-//     render()
-//     {
-//         return ReactDOM.createPortal(
-//             <div className="details-view-container shown" ref={ this.detailsViewRef }>
-
-//                 <BackButton onClick={ this.handleBackClick } />
-//                 <PokemonCard pokemon={ this.props.pokemon } />
-//                 <Details pokemon={ this.props.pokemon } />
-
-//             </div>, document.body
-//         )
-//     }
-// }
-
-// export default DetailsView;
