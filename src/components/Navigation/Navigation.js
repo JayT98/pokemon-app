5dcec5421f10a298ca9834895fd82033c7ced513
JayT98 <kientrung2509@gmail.com>
2023-05-07 15:04:37 +1000

    Refactor navigation with create generations as array

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,19 +3,68 @@ import NavigationLink from "./NavigationLink";
 import "./Navigation.css";
 
 function Navigation() {
+    const  generation = [
+        {
+            link: "gen-i",
+            text: "I",
+            offset: 0,
+            limit: 151
+        },
+        {
+            link: "gen-ii",
+            text: "II",
+            offset: 151,
+            limit: 100
+        },
+        {
+            link: "gen-iii",
+            text: "III",
+            offset: 251,
+            limit: 135
+        },
+        {
+            link: "gen-iv",
+            text: "IV",
+            offset: 386,
+            limit: 107
+        },
+        {
+            link: "gen-v",
+            text: "V",
+            offset: 493,
+            limit: 156
+        },
+        {
+            link: "gen-vi",
+            text: "VI",
+            offset: 649,
+            limit: 72
+        },
+        {
+            link: "gen-vii",
+            text: "VII",
+            offset: 721,
+            limit: 88
+        },
+        {
+            link: "gen-viii",
+            text: "VIII",
+            offset: 809,
+            limit: 89
+        }
+    ];
     return (
         <div className="navigation-container">
             <h3 className="navigation-title">Select Generation:</h3>
 
             <div className="links-container">
-                <NavigationLink exact to="/gen-i">I</NavigationLink>
-                <NavigationLink exact to="/gen-ii">II</NavigationLink>
-                <NavigationLink exact to="/gen-iii">III</NavigationLink>
-                <NavigationLink exact to="/gen-iv">IV</NavigationLink>
-                <NavigationLink exact to="/gen-v">V</NavigationLink>
-                <NavigationLink exact to="/gen-vi">VI</NavigationLink>
-                <NavigationLink exact to="/gen-vii">VII</NavigationLink>
-                <NavigationLink exact to="/gen-viii">VIII</NavigationLink>
+                {generation.map((gen, index) => {
+                    return (
+                        <NavigationLink exact to={`/${gen.link}`} key={index}>
+                            {gen.text}
+                        </NavigationLink>
+                    );
+                })}
             </div>
         </div>
     );
