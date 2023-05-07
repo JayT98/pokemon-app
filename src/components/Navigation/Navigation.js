362d32e3ef46a2031ed5f623d5a3ca0d2be942ec
JayT98 <kientrung2509@gmail.com>
2023-05-07 15:15:50 +1000

    Move generations array to external data folder and fixing the code in navigation file

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,67 +1,18 @@
 import React from "react";
 import NavigationLink from "./NavigationLink";
+import generations from "../../data/generations";
 import "./Navigation.css";
 
 function Navigation() {
-    const  generation = [
-        {
-            link: "gen-i",
-            text: "I",
-            offset: 0,
-            limit: 151
-        },
-        {
-            link: "gen-ii",
-            text: "II",
-            offset: 151,
-            limit: 100
-        },
-        {
-            link: "gen-iii",
-            text: "III",
-            offset: 251,
-            limit: 135
-        },
-        {
-            link: "gen-iv",
-            text: "IV",
-            offset: 386,
-            limit: 107
-        },
-        {
-            link: "gen-v",
-            text: "V",
-            offset: 493,
-            limit: 156
-        },
-        {
-            link: "gen-vi",
-            text: "VI",
-            offset: 649,
-            limit: 72
-        },
-        {
-            link: "gen-vii",
-            text: "VII",
-            offset: 721,
-            limit: 88
-        },
-        {
-            link: "gen-viii",
-            text: "VIII",
-            offset: 809,
-            limit: 89
-        }
-    ];
     return (
         <div className="navigation-container">
             <h3 className="navigation-title">Select Generation:</h3>
 
             <div className="links-container">
-                {generation.map((gen, index) => {
+                {generations.map(({id, link, text}) => {
                     return (
-                        <NavigationLink exact to={`/${gen.link}`} key={index}>
-                            {gen.text}
+                        <NavigationLink exact key={id} to={link}>
+                            {text}
                         </NavigationLink>
                     );
                 })}
--- src/components/Navigation/NavigationLink.js
+++ src/components/Navigation/NavigationLink.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavigationLink( props ) {
+function NavigationLink(props) {
     return (
         <NavLink
             className="navigation-link"
             activeClassName="active"
-            {...props}>
-            {props.children}
-        </NavLink>
+            {...props}
+        />
     );
 }
 
